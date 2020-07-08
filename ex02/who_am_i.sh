cc4ac2182b59e3a2015f9cda330678456f1cf404
Adin Nell <anell@c3r5s6.wethinkcode.co.za>
2020-07-08 10:27:09 +0200

    Update ex02 comments

--- ex02/who_am_i.sh
+++ ex02/who_am_i.sh
@@ -6,14 +6,14 @@
 #    By: anell <marvin@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/07/07 14:15:33 by anell             #+#    #+#              #
-#    Updated: 2020/07/08 10:00:19 by anell            ###   ########.fr        #
+#    Updated: 2020/07/08 10:26:34 by anell            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 ldapwhoami -Q | cut -c4-
 
 # First used ldapwhoami in my command line and it gave me the information regarding my login/ticket, however it gave me lots of added information that i did not need.
-# The -Q i searched for using man ldapwhoami and it says Enable SASL Quiet mode.
+# The -Q i searched for using man ldapwhoami and it says Enable SASL Quiet mode. This hides all the lines starting with SASl
 # I then used the cut function(cuts/deletes unwanted information) with the help of typing man cut in my command line because i wanted to get rid of unwanted information
 # In the man cut "-c" specifies the character position and the 4 is specifing from which position to cut 
 
